dc6e19c8f769208113a7f2943de9c59c0740aefe
Kirill Lee <keizo.lee@mail.ru>
2020-07-20 12:48:50 +0600

    finished todos

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur,
+      earum odit! Architecto cupiditate dolore, hic itaque nam officia perferendis
+      recusandae. Accusamus alias cumque eaque, eius iste mollitia nobis numquam sunt.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
